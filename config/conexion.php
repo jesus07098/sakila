96f1605ad27ed5e7d84d1153c8e833c34b2cd6a4
jesus07098 <jesus07098@gmail.com>
2020-08-12 14:47:17 -0400

    refactor: cambio en el archivo conexion_ftp.txt

--- config/conexion.php
+++ config/conexion.php
@@ -7,16 +7,31 @@ $ajustes = [
     3 => 2, //genera excepciones cuando hay errores en los querys
 ];
 
+//Valores de mi base de datos local (mi pc)//
+$host = "localhost";
+$dbname = "sakila";
+$usuario = "root";
+$password = "Daniel123*";
 
+/*
 define("DB_DRIVER", "mysql");
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "Daniel123*");
 define("DB_NAME", "sakila");
-try {
-    $conexion = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD, $ajustes);
-} catch (PDOException $exception) {
-    throw new Exception("Ha ocurrido un error al conectarnos a la Base de Datos {$exception->getMessage()}");
+*/
 
+//Valores de mi base de datos de 000webhost//
+if($_SERVER['SERVER_NAME'] === 'rubycode.000webhostapp.com') {
+    $host = "localhost";
+    $dbname = "id12551219_sakila";
+    $usuario = "id12551219_jesus";
+    $password = "_WZ]4s#pyN<GG-PQ";
 }
 
+try{
+    $conexion = new PDO("mysql:host={$host}; dbname={$dbname}", $usuario, $password);
+}catch(PDOException $exception){
+    throw new Exception("Hubo un error al conectarnos a la base de datos: {$exception->getMessage()}");
+    //array_push($errores, $exception -> getMessage());
+}
